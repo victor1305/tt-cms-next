406421fd18008f528e7430fb9f9fb5ac7e334033
Victor <victor.sanchez@phonr.com>
2023-09-22 08:04:19 +0200

    feat: home and first steps

---
+++ components/organisms/Principal/index.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { sections } from "@/lib/constants";
+
+import styles from "./Principal.module.scss";
+
+const Principal = () => (
+  <div className={styles["home-container"]}>
+    <div className={styles["home-container__login"]}>
+      <Image
+        src="/LogoTT.png"
+        width={140}
+        height={105}
+        alt="Logo TT"
+        priority
+      />
+      <h4>
+        CMS <span>TurfTipster</span>
+      </h4>
+    </div>
+
+    <div className={styles["home-container__buttons-container"]}>
+      {sections.map((elm, index) => (
+        <div key={index}>
+          <Link href={elm.to}>
+            <button className="btn btn--primary">{elm.copy}</button>
+          </Link>
+        </div>
+      ))}
+    </div>
+    <div className={styles["margin-bottom-box"]}></div>
+  </div>
+);
+
+export default Principal;
---
+++ components/organisms/index.js
@@ -0,0 +1,5 @@
+import Principal from './Principal';
+
+export {
+  Principal
+}
