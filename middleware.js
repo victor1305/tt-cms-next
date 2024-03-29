3d660a5d04b7953bbb2335fc667c8be90ab4a809
Victor <victor.sanchez@phonr.com>
2024-03-29 11:31:15 +0100

    fix: middleware

--- middleware.js
+++ middleware.js
@@ -6,23 +6,28 @@ export function middleware(request) {
   const url = request.nextUrl.clone();
 
   if (url.pathname.startsWith('/login')) {
-    // Si está intentando acceder a login pero ya tiene un token válido, redirigir a home.
     if (token) {
       try {
         const tokenData = jwtDecode(token.value);
-        if (tokenData.exp > Date.now() / 1000) {
-          if (url.pathname === '/login') {
-            // Verificar si realmente estamos en /login.
-            url.pathname = '/'; // Cambia la ruta a home.
-            return NextResponse.redirect(url);
-          }
+        if (tokenData.exp < Date.now() / 1000) {
+          // Token caducado, borra el token y sigue en login.
+          const response = NextResponse.redirect(url);
+          response.cookies.delete('token'); // Borra el token caducado.
+          return response;
+        } else if (url.pathname === '/login') {
+          // Token válido, redirige a home.
+          url.pathname = '/';
+          return NextResponse.redirect(url);
         }
       } catch (error) {
         console.error('Error decoding token:', error);
+        // En caso de error en la decodificación, considera borrar el token también.
+        const response = NextResponse.redirect(url);
+        response.cookies.delete('token'); // Borra el token potencialmente inválido.
+        return response;
       }
     }
   } else {
-    // Si intenta acceder a cualquier otra página y no está logueado, redirigir a login.
     if (!token) {
       url.pathname = '/login';
       return NextResponse.redirect(url);
@@ -35,6 +40,7 @@ export function middleware(request) {
 export const config = {
   matcher: [
     '/',
+    '/login', // Asegúrate de incluir login en el matcher.
     '/apuestas',
     '/apuestas/(.*)',
     '/clientes',
